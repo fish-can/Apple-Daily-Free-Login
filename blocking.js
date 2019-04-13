290f796bda2c23cbb694e0d377ae4d869282ddd6
Fishcan <umts111321@gmail.com>
2019-04-13 17:30:45 +0800

    Remove annotation

--- blocking.js
+++ blocking.js
@@ -1,7 +1,7 @@
 chrome.webRequest.onBeforeRequest.addListener(
   function () { return {cancel: true}; },
   {
-    urls: ['https://img.appledaily.com.tw/pay/js/paywall.20190410.js'], // Change this to a more specific pattern
+    urls: ['https://img.appledaily.com.tw/pay/js/paywall.20190410.js'],
     types: ['script']
   },
   ['blocking']
